c142184a2824fdfa12336faee87770ef38ba5859
Sam Morehouse <sam.morehouse@gmail.com>
2017-09-06 22:38:56 -0500

    initial revision

---
+++ src/main/java/com/cdk/demo/GreetingConfiguration.java
@@ -0,0 +1,13 @@
+package com.cdk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingConfiguration {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingConfiguration.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdk/demo/GreetingConfigurationTests.java
@@ -0,0 +1,16 @@
+package com.cdk.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GreetingConfigurationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
